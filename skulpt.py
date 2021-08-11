0fbdb64202b6b2b4495309156ec65d58cc273189
streetartist <60282562+streetartist@users.noreply.github.com>
2021-08-11 05:49:27 +0000

    Update skulpt.py

--- skulpt.py
+++ skulpt.py
@@ -1,11 +1,16 @@
 class Module:
     def __init__(self, name):
+        self.name = name
         self.maincode='''
 var $builtinmodule = function (name) {{
 	var mod = {{__name__: new Sk.builtin.str("{name}")}}
-
+        {func}
 	return mod;
-}
-        '''.format(name=name)
+}}
+        '''.format(name=self.name)
+
+    def generate():
+        
+        self.maincode=self.maincode.format(func=func_code)
 
  
