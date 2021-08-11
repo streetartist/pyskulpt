5674d46f4d4050f01132c92ef6f09ebcab5cc7a1
streetartist <60282562+streetartist@users.noreply.github.com>
2021-08-11 06:00:41 +0000

    Update skulpt.py

--- skulpt.py
+++ skulpt.py
@@ -1,3 +1,17 @@
+def del_waste(self, wastes):
+    waste = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__',
+'__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'build', 'generate',]
+    
+    production = []
+
+    for i in wastes:
+        j = getattr(self, i)
+
+        if i not in waste and hasattr(j, "litchi") == True:
+            production.insert(0, j)
+
+    return production
+
 class Module:
     def __init__(self, name):
         self.name = name
@@ -10,7 +24,8 @@ var $builtinmodule = function (name) {{
         '''.format(name=self.name)
 
     def generate():
-        
+        func_code = self.build() if self.build() != None else del_waste(self, dir(self))
+
         self.maincode=self.maincode.format(func=func_code)
 
  
