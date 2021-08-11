bc083220e817dbb3d30cdccdacce057b2186f3fe
streetartist <60282562+streetartist@users.noreply.github.com>
2021-08-11 06:53:47 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,10 @@
 from pyskulpt import Module
 
 class mod(Module):
+    def __init__(self):
+        self.name= "mod"
     def build():
         # 使用Sk.builtin.func能让python理解这是个函数
-	mod.add = Sk.builtin.func(lambda a, b: return Sk.ffi.remapToJs(a) + Sk.ffi.remapToJs(b))
+        mod.add = Sk.builtin.func(lambda a, b: Sk.ffi.remapToJs(a) + Sk.ffi.remapToJs(b))
+m = mod().generate()
+print(m)
